c45e278eb5e404ad38e74518ff100a96e469aa9b
Govindaspkta <sapgovinda05@gmail.com>
2025-08-05 15:55:19 +0545

    learn leanr learn

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from .models import Task
+
 # Create your views here.
 def addtask(request):
     task=request.POST['task']
@@ -8,4 +9,5 @@ def addtask(request):
     return redirect('homepage')
 
 def mark_as_done(request,pk):
-    return HttpResponse(pk)
+    task= get_object_or_404(Task,pk=pk)
+    return HttpResponse(task)
