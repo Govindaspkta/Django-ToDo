4a26210ef51a784df6524a512f053966d8099f15
Govindaspkta <sapgovinda05@gmail.com>
2025-08-05 13:09:40 +0545

    template created

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import  render
 
 def home(request):
-    return HttpResponse('<h3>THIS IS A HOMEPAGE</H1>')
+    return render(request, 'home.html')
