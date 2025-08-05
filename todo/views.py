c59c3836c0a83a068d695845e5de100bbbe7053e
Govindaspkta <sapgovinda05@gmail.com>
2025-08-05 16:18:39 +0545

    learn learn learn

--- todo/views.py
+++ todo/views.py
@@ -10,4 +10,14 @@ def addtask(request):
 
 def mark_as_done(request,pk):
     task= get_object_or_404(Task,pk=pk)
-    return HttpResponse(task)
+    task.is_completed=True
+    task.save()
+    
+    return redirect('homepage')
+        
+def mark_as_undone(request,pk):
+    task=get_object_or_404(Task,pk=pk)
+    task.is_completed=False
+    task.save()
+    
+    return redirect('homepage')
