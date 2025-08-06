a4fc74e5b62f27d7798c1e2f0dac0759041d3c62
Govindaspkta <sapgovinda05@gmail.com>
2025-08-06 10:03:06 +0545

    leanr learnlean

--- todo/views.py
+++ todo/views.py
@@ -22,4 +22,20 @@ def mark_as_undone(request,pk):
     
     return redirect('homepage')
 def edit_task(request,pk):
-    return render(request,'edit_task.html')
+    get_task=get_object_or_404(Task,pk=pk)
+    if request.method =='POST':
+        new_task=request.POST['task']
+        get_task.task=new_task
+        get_task.save()
+        
+        return redirect('homepage')
+    else:
+        context={
+            'get_task':get_task
+        }
+        return render(request,'edit_task.html',context)
+def delete_task(request,pk):
+     task=get_object_or_404(Task,pk=pk)
+     task.delete()
+    #  task.save()
+     return redirect('homepage')
